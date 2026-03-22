4de82dfcc02d9b4484694f7f148b1a069d033f4a
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-03-22 16:06:06 -0300

    // 1. Cria a ViewModel // 2. Conecta a ViewModel á Tela (View)            // 3.Cria o motor e inscreve a ViewModel nele.

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -8,16 +8,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FinanceiroObserver.App.Services;
+using FinanceiroObserver.App.ViewModels;
 
 namespace FinanceiroObserver.App;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+        
+        // 1. Cria a ViewModel
+        var viewModel = new MonitorAcoesViewModel();
+        
+        // 2. Conecta a ViewModel á Tela (View)
+        this.DataContext = viewModel;
+       
+        // 3.Cria o motor e inscreve a ViewModel nele
+        var motor = new MotorMercado();
+        motor.Inscrever(viewModel);
     }
 }
